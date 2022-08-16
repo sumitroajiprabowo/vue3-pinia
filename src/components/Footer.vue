4e7e4abda90a0efe74213f21614e31fa059ef829
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-16 22:13:08 +0700

    Initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+This is footer
+
+<button @click="increment">Increment</button>
+
+<button @click="decrement">Decrement</button>
+
+<button @click="reset">Reset</button>
+</template>
+
+<script setup>
+
+import { useCounterStore } from '../stores/counter';
+
+const counter = useCounterStore();
+const increment = () => {
+    counter.$patch((state) => {
+        state.count++;
+    });
+}
+
+const decrement = () => {
+    counter.$patch((state) => {
+        state.count--;
+    });
+}
+
+const reset = () => {
+    counter.$patch((state) => {
+        state.count = 0;
+    });
+}
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+This is header
+<pre>
+    {{ counter }}
+</pre>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useCounterStore } from '../stores/counter';
+
+const counterStore = useCounterStore();
+const counter = computed(() => counterStore.count);
+
+counterStore.$subscribe((mutation, state) => {
+    console.log(mutation, state);
+});
+</script>
