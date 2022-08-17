6be2bec0eb4e9be047472127c4df50b1d214d4b3
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-08-17 15:39:32 +0700

    Getters

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,16 @@
 <template>
 This is header
 <pre>
-    {{ counter }}
+   state : {{ counter }}
+</pre>
+<pre>
+   getters : {{ getCount }}
+</pre>
+<pre>
+    getters : {{ doubleCount }}
+</pre>
+<pre>
+    getters : {{ devideByTwo }}
 </pre>
 </template>
 
@@ -11,8 +20,12 @@ import { useCounterStore } from '../stores/counter';
 
 const counterStore = useCounterStore();
 const counter = computed(() => counterStore.count);
+const getCount = computed(() => counterStore.getCount);
+const doubleCount = computed(() => counterStore.doubleCount);
+const devideByTwo = computed(() => counterStore.devideBy(2));
 
 counterStore.$subscribe((mutation, state) => {
     console.log(mutation, state);
 });
+
 </script>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,9 +2,12 @@ import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore({
    id : "counter",
-
     state : () => ({
         count : 0
     }),
-
+    getters : {
+        getCount : state => state.count,
+        doubleCount : state => state.count * 2,
+        devideBy: state => (divider) => state.count / divider
+    }
 });
